88d7f5120be2a95b9d6cf3eeb66eb04e9e3f51de
Stefan Saoulis <60780331+SSaoulis@users.noreply.github.com>
2026-01-07 13:58:30 +0000

    moved down

--- utils/test_camera.py
+++ utils/test_camera.py
@@ -46,7 +46,7 @@ def take_photo(output_dir: str = ".", filename: str = None) -> str:
     CROP_W, CROP_H = 640, 640
 
     ANCHOR_X_FRAC = 4 / 12
-    ANCHOR_Y_FRAC = 2 / 5 
+    ANCHOR_Y_FRAC = 3 / 5 
 
     crop_x = int(SENSOR_W * ANCHOR_X_FRAC)
     crop_y = int(SENSOR_H * ANCHOR_Y_FRAC)
